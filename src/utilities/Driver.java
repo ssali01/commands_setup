25151f5b1155cb471108cf339559056b7d648ea2
Salma Sali <salianovaj@gmail.com>
2022-11-16 06:39:26 -0500

    Driver class was added and BrowserUtils was deleted

---
+++ src/utilities/Driver.java
@@ -0,0 +1,10 @@
+package src.utilities;
+
+public class Driver {
+    // new class
+    // connect to browser
+    // changes
+// Unversioned means the file is not being kept tracked of in terms of the changes
+// we need to stage it in order to keep track those files
+
+}
